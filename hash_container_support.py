c9de986066957aa9ee12ae64964edd7928c48b8d
David Mentler <david.mentler@shapr3d.com>
2024-03-01 02:10:32 +0100

    Introduce new pseudo-synthetic type to simplify single value synthetics

--- hash_container_support.py
+++ hash_container_support.py
@@ -176,6 +176,22 @@ class IterableContainer:
 	def iterator(self):
 		return None
 
+class Value:
+	"""Interface class describing required functionality for use with ValueContainerSynthetic"""
+
+	def update(self):
+		return
+
+	def validate(self):
+		return
+
+	def get(self):
+		return None
+
+	def get_summary(self):
+		return ""
+
+
 class IterableContainerSynthetic:
 	"""Implementation of the LLDB SyntheticChildrenProvider for IterableContainers"""
 
@@ -249,6 +265,67 @@ class IterableContainerSynthetic:
 			return f"<Error: {self.error}>"
 
 		return self.container.get_summary()
+
+class ValueSynthetic:
+	"""Implementation of the LLDB SyntheticChildrenProvider for Values"""
+
+	def __init__(self, value, rename_to=None):
+		self.value = value
+		self.rename_to = rename_to
+
+		self.update()
+
+	def update(self):
+		self.error = None
+
+		self.content = None
+		self.content_set = False
+
+		# Let underlying value update if it is stateful
+		self.value.update()
+
+		# Offer ability to opt-out of inspecting the value if appears invalid
+		self.error = self.value.validate()
+
+	def populate(self):
+		if self.content_set:
+			return
+
+		if not self.error:
+			content = self.value.get()
+
+			if content and self.rename_to:
+				content = rename_valobj(content, self.rename_to)
+
+			self.content = content
+
+		self.content_set = True
+
+	def has_children(self):
+		return self.error is None
+
+	def num_children(self):
+		self.populate()
+
+		return self.content and 1 or 0
+
+	def get_child_at_index(self, index):
+		self.populate()
+
+		if index != 0:
+			return None
+
+		return self.content
+
+	def get_child_index(self, name):
+		return None
+
+	def get_summary(self):
+		if self.error:
+			return f"<Error: {self.error}>"
+
+		return self.value.get_summary()
+
 
 class SortingSyntheticAdapter(SyntheticAdapter):
 	def __init__(self, wrapped, is_map):
