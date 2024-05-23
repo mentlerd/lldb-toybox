c8ce695b67e0a6066a601ce8693f212aedcaa025
David Mentler <david.mentler@shapr3d.com>
2024-05-24 00:29:58 +0200

    Command infra

--- lldb_toybox/lib/registry.py
+++ lldb_toybox/lib/registry.py
@@ -1,13 +1,75 @@
 
 import lldb
 
+import argparse
 import re
+import shlex
 
 _synthetics = list()
+_initializers = list()
+
+_main_parser = argparse.ArgumentParser(
+	prog='toybox',
+	description='''
+		A swiss army knife toolset for anything LLDB related with the goal of
+		making debugging fun and enjoyable process.
+
+		Focuses mostly on working around/fixing idiosyncrasies of Xcode
+	'''
+)
+_main_subparser = _main_parser.add_subparsers(required=True)
+
+class Command:
+	def __init__(self, name=None):
+		self.name = name
+		pass
+
+	def __call__(self, clazz):
+		subparser = _main_subparser.add_parser(self.name)
+		subparser.set_defaults(impl=clazz(subparser))
 
 def Synthetic(clazz):
 	_synthetics.append(clazz)
 	return clazz
+
+def Initializer(func):
+	_initializers.append(func)
+	return func
+
+
+def deploy(debugger):
+	deploy_main_command(debugger)
+
+	for clazz in _synthetics:
+		category_name = f'lldb-toybox.{clazz.category}'
+		category = debugger.GetCategory(category_name)
+
+		if not category.IsValid():
+			category = debugger.CreateCategory(category_name)
+			category.AddLanguage(lldb.eLanguageTypeC_plus_plus)
+			category.SetEnabled(True)
+
+		deploy_synthetic(category, clazz)
+
+	deploy_init_hook(debugger)
+
+
+def deploy_main_command(debugger):
+	import sys
+
+	# Instantiate main command in this module
+	def main_command(debugger, command, result, internal_dict):
+		args = _main_parser.parse_args(shlex.split(command))
+		impl = args.impl
+		del args.impl
+
+		impl and impl.run(args)
+
+	sys.modules[__name__].main_command = main_command
+
+	# Register it
+	debugger.HandleCommand('command script add -o -f {}.main_command toybox'.format(__name__))
+
 
 def deploy_synthetic(category, clazz):
 	class_name = f"{clazz.__module__}.{clazz.__qualname__}"
@@ -32,21 +94,8 @@ def deploy_synthetic(category, clazz):
 		category.AddTypeSynthetic(recognizer, synthetic)
 		category.AddTypeSummary(recognizer, summary)
 
-def deploy(debugger):
 
-	# Register synthetics we know about
-	for clazz in _synthetics:
-		category_name = f'lldb-toybox.{clazz.category}'
-		category = debugger.GetCategory(category_name)
-
-		if not category.IsValid():
-			category = debugger.CreateCategory(category_name)
-			category.AddLanguage(lldb.eLanguageTypeC_plus_plus)
-			category.SetEnabled(True)
-
-		deploy_synthetic(category, clazz)
-
-	# Register a stop hook for deferred initialization of things that depend on the target
+def deploy_init_hook(debugger):
 	result = lldb.SBCommandReturnObject()
 	debugger.GetCommandInterpreter().HandleCommand(f'target stop-hook add -P {__name__}.LateInitStopHook', result)
 
@@ -57,9 +106,7 @@ class LateInitStopHook:
 	def __init__(self, target, extra_args, dict):
 		pass
 
-	def disable(self, exe_ctx):
-		debugger = exe_ctx.GetTarget().GetDebugger()
-
+	def disable(self, debugger):
 		result = lldb.SBCommandReturnObject()
 		interpreter = debugger.GetCommandInterpreter()
 		interpreter.HandleCommand('target stop-hook list', result)
@@ -75,11 +122,12 @@ class LateInitStopHook:
 		debugger.HandleCommand(f'target stop-hook disable {match.group(1)}')
 
 	def handle_stop(self, exe_ctx, stream):
-		print("lldb-toybox is performing late-initialization, hold tight...", end=" ")
+		print("lldb-toybox is performing late-initialization, hold tight...")
+		debugger = exe_ctx.GetTarget().GetDebugger()
 
-		# First time initialization should be done once
-		self.disable(exe_ctx)
+		# Ensure we won't run again for this target
+		self.disable(debugger)
 
-		# Scan target for interesting types we might have synthetics for
-		
-		
+		# Run initialization routines
+		for func in _initializers:
+			func(debugger, exe_ctx.GetTarget())
