d420928aca5a133346336a70c304e12d5830e09f
David Mentler <david.mentler@shapr3d.com>
2024-07-06 10:23:26 +0200

    Utility for caching per-process/stop data across synthetics

--- lldb_toybox/lib/utils.py
+++ lldb_toybox/lib/utils.py
@@ -49,3 +49,56 @@ def is_prime(number):
 			return False
 
 	return True
+
+class Cache:
+	"""Simple function decorator which caches the return value per process/stop"""
+
+	def __init__(self, per=None):
+		if not per or per not in ['process', 'stop']:
+			raise RuntimException()
+
+		self.per = per
+
+		self.cache = dict()
+		self.last = None
+
+	def __call__(self, func):
+		def wrapped(obj, *args, **kwargs):
+			proc = obj
+
+			if not isinstance(proc, lldb.SBProcess):
+				proc = obj.GetProcess()
+
+			if not isinstance(proc, lldb.SBProcess):
+				raise RuntimException()
+
+			uid = proc.GetUniqueID()
+
+			# Evict stale data when accesing a different process
+			if self.last != uid:
+				self.last = uid
+
+				for uid, entry in list(self.cache.items()):
+					if entry[0].GetNumThreads() != 0:
+						continue
+
+					del self.cache[uid]
+
+			# Enrich context based on operation mode
+			context = 0
+
+			if self.per == 'stop':
+				context = proc.GetStopID()
+
+			# Check cache for compatible entry
+			entry = self.cache.get(uid)
+
+			if entry and entry[1] == context:
+				return entry[2]
+
+			# Otherwise compute it
+			self.cache[uid] = (proc, context, func(obj, *args, **kwargs))
+
+			return self.cache[uid][2]
+
+		return wrapped
